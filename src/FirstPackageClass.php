4819e991b1b17fd596cc9db24c931dcf1ff3d16a
leovujanic <leonard.vujanic@gmail.com>
2017-10-14 17:04:13 +0200

    Initial commit

---
+++ src/FirstPackageClass.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: leonardvujanic
+ * Date: 14/10/2017
+ * Time: 16:33
+ */
+
+namespace leovujanic\firstpack;
+
+/**
+ * Class FirstPackageClass
+ */
+class FirstPackageClass
+{
+    
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return static::class;
+    }
+}
